5c6cd97c014e181050f9479c6f091cdc7031c6d9
Svayam Mehta <svayammehta2004@gmail.com>
2025-03-01 10:00:42 +0530

    Basic frontend Ready

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import Workout from './components/Workout'
 import Generator from './components/Generator'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [workout, setWorkout] = useState(null)
+  const [poison, setPoison] = useState('individual')
+  const [muscles, setMuscles] = useState([])
+  const [goals, setGoals] = useState('strength_power')  
 
   return (
     <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base' >
      <Hero/>
-     <Workout/>
-     <Generator/>
+     <Generator poison={poison} setPoison={setPoison} muscles={muscles} setMuscles={setMuscles} goals={goals} setGoals={setGoals}/>
+     {workout && (<Workout workout={workout}/>)}
     </main>
   )
 }
