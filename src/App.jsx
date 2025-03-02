2f40b95edb11ba3cf2f470bf1ec031c7f9364b48
Svayam Mehta <svayammehta2004@gmail.com>
2025-03-02 20:08:14 +0530

    Webpage ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
 import { useState } from 'react'
 import Hero from './components/Hero'
-import Workout from './components/Workout'
 import Generator from './components/Generator'
+import Workout from './components/Workout'
+import { generateWorkout } from './utils/functions'
+
 
 function App() {
   const [workout, setWorkout] = useState(null)
   const [poison, setPoison] = useState('individual')
   const [muscles, setMuscles] = useState([])
-  const [goals, setGoals] = useState('strength_power')  
+  const [goal, setGoal] = useState('strength_power')
+
+  function updateWorkout() {
+    if (muscles.length < 1) {
+      return
+    }
+    let newWorkout = generateWorkout({ poison, muscles, goal })
+    setWorkout(newWorkout)
+
+    window.location.href = '#workout'
+  }
 
   return (
-    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base' >
-     <Hero/>
-     <Generator poison={poison} setPoison={setPoison} muscles={muscles} setMuscles={setMuscles} goals={goals} setGoals={setGoals}/>
-     {workout && (<Workout workout={workout}/>)}
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
+      <Hero />
+      <Generator
+        poison={poison}
+        setPoison={setPoison}
+        muscles={muscles}
+        setMuscles={setMuscles}
+        goal={goal}
+        setGoal={setGoal}
+        updateWorkout={updateWorkout}
+      />
+      {workout && (<Workout workout={workout} />)}
     </main>
   )
 }
